d33dd6b50057acc203c3be004c3d2915bc8f3572
User <bulentguneysu@hotmail.com>
2023-10-16 17:45:39 +0300

    first commit

---
+++ src/test/java/com/eaapp/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package com.eaapp.pages;
+
+import com.eaapp.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy (id= "loginLink")
+    public WebElement loginButton;
+
+    @FindBy (xpath = "//input[@data-val-required='The UserName field is required.']")
+    public WebElement userNameBox;
+
+    @FindBy (xpath = "//input[@data-val-required='The Password field is required.']")
+    public WebElement passWordBox;
+
+    @FindBy (xpath = "//input[@type='submit']")
+    public WebElement loginBox;
+
+
+
+
+    public void loginMethod (){
+        loginButton.click();
+        String userName= ConfigurationReader.get("username");
+        String passWord= ConfigurationReader.get("password");
+        userNameBox.sendKeys(userName);
+        passWordBox.sendKeys(passWord);
+        loginBox.click();
+    }
+
+}
